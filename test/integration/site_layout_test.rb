7e26dac116980ea09f670766477c8255e25059ac
odawara <w32119@scsk.jp>
2017-03-09 10:15:30 +0900

    Q-Apeの暫定ロゴをHome画面に追加。また、邪魔なのでRailsロゴを非表示にする。

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,7 +9,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", root_path, count: 0
     assert_select "a[href=?]", home_path, count: 2
     assert_select "a[href=?]", help_path, count: 1
-    assert_select "a[href=?]", about_path, count: 1
+    assert_select "a[href=?]", about_path, count: 2
     assert_select "a[href=?]", contact_path, count: 1
   end
 
