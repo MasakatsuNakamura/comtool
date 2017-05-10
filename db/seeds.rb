27071f3ac0384f7ee1384d64cb339d6841936fed
odawara <w32119@scsk.jp>
2017-05-10 13:47:37 +0900

    db:seed 実行時、マスターテーブルに同じデータが複数生成される不具合を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-CommunicationProtocol.create!(name: 'CAN', protocol_number: "1")
-QinesVersion.create!(name: 'V1.0', qines_version_number: "1")
-QinesVersion.create!(name: 'V2.0', qines_version_number: "2")
-Project.create!(name: 'TestProject', communication_protocol_id: '1', qines_version_id: '1')
+CommunicationProtocol.find_or_create_by(name:'CAN') {|v| v.protocol_number = '1'}
+
+QinesVersion.find_or_create_by(name:'V1.0') {|v| v.qines_version_number = '1'}
+QinesVersion.find_or_create_by(name:'V2.0') {|v| v.qines_version_number = '2'}
+
 DatabaseManage.delete_all
 # DatabaseManage.connection.execute("TRUNCATE TABLE DatabaseManage;")   # TRUNCATEはsqlite3はエラー
 DatabaseManage.create!(backup_file_path: 'CAN/test',backup_date: Date.today, project_id: 1)
