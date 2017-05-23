8e8afe2235e74bcb21989e8a920da9b811ab1c26
Masakatsu Nakamura <w41283@scsk.jp>
2017-05-23 13:58:47 +0900

    調停編集機能をアップデート

--- app/models/mode.rb
+++ app/models/mode.rb
@@ -1,8 +1,16 @@
 class Mode < ApplicationRecord
   belongs_to :project
 
+  after_initialize :set_default, if: :new_record?
+
   validates :title,
             presence: true,
             uniqueness: { case_sensitive: false, scope: :project_id },
             length: { maximum: 50 }
+
+  private
+
+  def set_default
+    self.param = "条件1:\r\n    condition: AAA == BBB\r\n    true: アクション1\r\n    false: アクション2\r\n"
+  end
 end
